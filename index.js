f3edc3343b2888414d35e0930609007300c36478
Braydon <BADelgado2003@outlook.com>
2022-09-13 16:32:10 -0400

    commit tree 1472321ad8196ddbd4c985fd7f2f473745d78fa3

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching(array, name) {
+    const newArr = array.filter(driver => name.toLowerCase() === driver.toLowerCase())
+  
+    return newArr
+}
+
+function fuzzyMatch(array, name){
+    const newArray = array.filter(driver => name.substring(0, 1) === driver.substring(0, 1));
+
+    return newArray
+}
+
+const carDrivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+function matchName(array, name){
+    return array.filter(o =>
+        Object.keys(o).some(k => String(o[k]).toLowerCase().includes(name.toLowerCase())));
+}
